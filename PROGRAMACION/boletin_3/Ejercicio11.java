45832dd51bee0d7870859353e7ecc49f193a9050
oscar <you@example.com>
2024-11-14 13:03:30 +0100

    boletin-update

--- PROGRAMACION/boletin_3/Ejercicio11.java
+++ PROGRAMACION/boletin_3/Ejercicio11.java
@@ -29,6 +29,20 @@ import java.lang.Math;
 public class Ejercicio11 {
 
     /**
+     * Realiza el factorial de un número de forma recursiva.
+     * 
+     * @param num Número al que se le va a realizar el factorial.
+     * @return El factorial del número.
+     */
+
+    public static int factorialRec(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        return num * factorialRec(num - 1);
+    }
+
+    /**
      * Realiza el factorial de un número.
      * 
      * @param num Número al que se le va a realizar el factorial.
@@ -63,6 +77,7 @@ public class Ejercicio11 {
         valorCoseno += Ejercicio6.potencia(x, 8) / factorial(8);
         valorCoseno -= Ejercicio6.potencia(x, 10) / factorial(10);
         valorCoseno += Ejercicio6.potencia(x, 12) / factorial(12);
+        valorCoseno += Ejercicio6.potencia(x, 14) / factorial(14);
 
         return valorCoseno;
     }
@@ -72,18 +87,25 @@ public class Ejercicio11 {
         for (double i = 0.1; i <= 1; i += 0.1) {
             System.out.printf("El coseno de %.1f es: %8.5f", i, coseno(i));
             System.out.println();
-        };
+        }
+        ;
     };
 
     public static void bucleCosenoError() {
-        for (double i = 0.1; i <= 1; i += 0.1) {
-            System.out.printf("El error absoluto es: %8.5f", coseno(i) - Math.cos(i));
-            System.out.println();
-        }
+        System.out.printf("El error absoluto es: %8.5f", coseno(0.2) - Math.cos(0.2));
+        System.out.println();
     }
+
     public static void main(String[] args) {
         bucleCoseno();
+
         System.out.println();
+        
         bucleCosenoError();
+
+        System.out.println();
+
+        System.out.println("El factorial con la función con un bucle es: " + factorial(5));
+        System.out.println("El factorial con la función recursiva es: " + factorialRec(5));
     }
 }

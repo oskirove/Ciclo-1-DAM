f7aa105e5117a1d08b61d72269073d2b60b62512
oscar <you@example.com>
2025-01-22 17:18:24 +0100

    boletin-update

---
+++ PROGRAMACION/evaluacion2/Boletin_Arrays/Ejercicio4/Ejercicio.java
@@ -0,0 +1,118 @@
+package PROGRAMACION.evaluacion2.Boletin_Arrays.Ejercicio4;
+
+public class Ejercicio {
+
+    /**
+     * Muestra cada caracter de una palabra en una linea diferente.
+     * 
+     * @param word palabra la cual mostraremos sus caracteres.
+     */
+
+    public static void muestraEnLinea(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
+
+    /**
+     * Extrae e imprime una subcadena de la cadena dada comenzando en el índice
+     * especificado
+     * y abarcando el número de caracteres especificado.
+     *
+     * @param cadena        la cadena original de la cual se extrae la subcadena
+     * @param inicio        el índice de inicio de la subcadena
+     * @param numCaracteres el número de caracteres a incluir en la subcadena
+     */
+
+    public static void subCadena(String cadena, int inicio, int numCaracteres) {
+        if (inicio > cadena.length() || numCaracteres > cadena.length()) {
+            System.out.println("");
+        } else {
+            int largo = inicio + numCaracteres;
+            for (int i = inicio; i < largo; i++) {
+                System.out.print(cadena.charAt(i));
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Muestra una cadena centrada en la terminal.
+     * 
+     * @param cadena palabra o frase que queremos centrar en la terminal.
+     */
+    public static void muestraCentrado(String cadena) {
+
+        int operacion = 68 - (cadena.length() / 2);
+        System.out.println();
+
+        for (int i = 0; i <= operacion; i++) {
+            System.out.print(" ");
+        }
+        System.out.print(cadena);
+
+        for (int i = 0; i <= operacion; i++) {
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Convierte una cadena de texto a un vector.
+     * 
+     * @param cadena cadena de texto que convertimos a vector.
+     * @return el vector con los caracteres de la cadena de texto.
+     */
+    public static char[] cadenaAVector(String cadena) {
+        char[] vector = new char[cadena.length()];
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = cadena.charAt(i);
+            // System.err.println(vector[i]);
+        }
+        return vector;
+    };
+
+    /**
+     * Muestra una cadena de texto empezando por el final.
+     * @param cadena es la cadena de texto que mostraremos del revés
+     */
+    public static void alReves(String cadena) {
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            System.out.print(cadena.charAt(i));
+        }
+        System.out.println();
+    }
+
+    /**
+     * Muestra por pantalla una cantidad indefinida de parámetros y los devuelve en mayuscula y concatenados.
+     * @param cadenas son las cadenas que queremos convertir.
+     * @return las cadenas en mayuscula y concatenadas.
+     */
+
+    public static String pasoAMayusculas(String... cadenas){
+        StringBuilder sb = new StringBuilder();
+
+        for(String cadena : cadenas){
+            for(int i = 0; i < cadena.length(); i++){
+                char letra = cadena.charAt(i);
+                if(letra == '_'){
+                    sb.append(' ');
+                } else if(letra >= 'a' || letra <= 'z'){
+                    sb.append((char) (letra - 'a' + 'A'));
+                } else {
+                    sb.append(letra);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.print(pasoAMayusculas("hola_mundo", "esto_es_una_prueba"));
+        System.out.println();
+        System.out.println();
+    }
+}
---
+++ PROGRAMACION/evaluacion2/Boletin_Arrays/Ejercicio4/enunciado.md
@@ -0,0 +1,14 @@
+> [!NOTE]
+> Crea una clase con los siguientes métodos estáticos públicos que serán realizados usando sólo los métodos `charAt()` y `length()`:
+
+1. **muestraEnLinea**: Método que muestre cada letra de una cadena que se pasa como parámetro en una línea distinta.
+
+2. **subCadena**: Método al que se le pase una cadena, una posición de inicio y una cantidad de caracteres y devuelve el fragmento indicado. Si se le pasan parámetros no válidos (índices fuera de rango o cadena `null`) devuelve cadena vacía.
+
+3. **muestraCentrado**: Función que se le pasa un `string` y lo muestra centrado en la consola (supón consola de 80 caracteres de ancho).
+
+4. **cadenaAVector**: Función a la que se le pasa una cadena y devuelve un vector de `char` con cada uno de los caracteres de la cadena.
+
+5. **alReves**: Función a la que se le pasa una frase y la devuelve en orden inverso.
+
+6. **pasoAMayusculas**: Función a la que se le pasa un número indeterminado de cadenas como parámetros y las devuelve concatenadas y todo en mayúsculas dentro del ASCII estándar de 7 bits (es decir, no tengas en cuenta vocales con tilde ni diéresis ni la letra Ñ), además si la cadena tiene un guión bajo (`_`) colocará un espacio en su lugar. Revisa el Apéndice II para hacer este apartado.

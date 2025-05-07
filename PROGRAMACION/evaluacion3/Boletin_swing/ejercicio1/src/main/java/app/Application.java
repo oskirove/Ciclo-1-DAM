6bf8ee74a231b5ff7fe8c1d4c99f5cbdd75a758c
oscar <you@example.com>
2025-05-07 17:27:49 +0200

    boletin update

--- PROGRAMACION/evaluacion3/Boletin_swing/ejercicio1/src/main/java/app/Application.java
+++ PROGRAMACION/evaluacion3/Boletin_swing/ejercicio1/src/main/java/app/Application.java
@@ -1,8 +1,49 @@
-
 package app;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Application {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    JFrame frame = new JFrame("Aplicación dado");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(600, 400);
+    frame.setLayout(new FlowLayout());
+
+
+    JLabel label = new JLabel("Genera un número aleatorio entre 1 y el número que introduzcas");
+    frame.add(label);
+    JTextField textField = new JTextField(10);
+    frame.add(textField);
+    JButton button = new JButton("Lanzar dado");
+    frame.add(button);
+    JLabel result = new JLabel();
+    frame.add(result);
+
+    button.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(java.awt.event.ActionEvent e) {
+        int maxRange = 6;
+
+        try {
+
+          String inputValue = textField.getText();
+          maxRange = Integer.parseInt(inputValue);
+
+          if (maxRange <= 1) {
+            maxRange = 6;
+          }
+          
+        } catch (NumberFormatException ex) {
+          maxRange = 6;
+        }
+
+        int randomNumber = (int) (Math.random() * maxRange) + 1;
+        result.setText("Resultado " + randomNumber);
+      }
+    });
+
+    frame.setVisible(true);
   }
 }
